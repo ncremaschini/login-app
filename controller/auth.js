449c614dc74ace59e4ecc851e49d6380106f67ca
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2021-06-06 22:27:47 +0200

    first commit

---
+++ controller/auth.js
@@ -0,0 +1,143 @@
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const AWS = require("aws-sdk");
+const request = require("request");
+const jwkToPem = require("jwk-to-pem");
+const jwt = require("jsonwebtoken");
+global.fetch = require("node-fetch");
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const poolData = {
+  UserPoolId: process.env.COGNITO_USER_POOL_ID, 
+  ClientId: process.env.COGNITO_CLIENT_ID, 
+};
+const pool_region = process.env.COGNITO_POOL_REGION;
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function validateToken(callback) {
+    var cognitoUser = userPool.getCurrentUser();
+    console.log('logged cognito user:', cognitoUser);
+    
+    request({
+        url: `https://cognito-idp.${pool_region}.amazonaws.com/${poolData.UserPoolId}/.well-known/jwks.json`,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            pems = {};
+            var keys = body['keys'];
+            for(var i = 0; i < keys.length; i++) {
+                //Convert each key to PEM
+                var key_id = keys[i].kid;
+                var modulus = keys[i].n;
+                var exponent = keys[i].e;
+                var key_type = keys[i].kty;
+                var jwk = { kty: key_type, n: modulus, e: exponent};
+                var pem = jwkToPem(jwk);
+                pems[key_id] = pem;
+            }
+            //validate the token
+            var decodedJwt = jwt.decode(localStorage.getItem('accessToken'), {complete: true});
+            if (!decodedJwt) {
+                console.log("Not a valid JWT token");
+                return;
+            }
+
+            var kid = decodedJwt.header.kid;
+            var pem = pems[kid];
+            if (!pem) {
+                console.log('Invalid token');
+                return;
+            }
+
+            jwt.verify(localStorage.getItem('accessToken'), pem, function(err, payload) {
+                if(err) {
+                    console.log("Invalid Token.");
+                } else {
+                    console.log("Valid Token.");
+                    console.log(payload);
+                }
+            });
+        } else {
+            console.log("Error! Unable to download JWKs");
+        }
+
+        callback("validation end");
+    });
+}
+
+function login(username, password, callback) {
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username : username,
+        Password : password,
+    });
+
+    var userData = {
+        Username : username,
+        Pool : userPool
+    };
+
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    console.log('authenticating %s via cognito...', username);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            localStorage.setItem('accessToken',result.getAccessToken().getJwtToken());
+            localStorage.setItem('idToken',result.getIdToken().getJwtToken());
+            localStorage.setItem('refreshToken',result.getRefreshToken().getToken());
+            
+            callback(result);
+        },
+        onFailure: function(err) {
+            callback(err);
+        },
+
+    });
+}
+
+function logout(callback) {
+    var cognitoUser = userPool.getCurrentUser();
+    
+    console.log('logged cognito user:', cognitoUser);
+
+    if (cognitoUser != null) {
+        cognitoUser.signOut();
+    }
+
+    localStorage.clear();
+
+    callback('success');
+}
+
+function refreshToken(callback) {    
+    var cognitoUser = userPool.getCurrentUser();
+    console.log('logged cognito user:', cognitoUser);
+   
+    const RefreshToken = new AmazonCognitoIdentity.CognitoRefreshToken({RefreshToken: localStorage.getItem('refreshToken')});
+    
+    cognitoUser.refreshSession(RefreshToken, (err, session) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let retObj = {
+                "access_token": session.accessToken.jwtToken,
+                "id_token": session.idToken.jwtToken,
+                "refresh_token": session.refreshToken.token,
+            }
+            console.log(retObj);
+        }
+    });
+
+    callback('done');
+}
+
+module.exports={
+    login,
+    logout,
+    validateToken,
+    refreshToken
+}
---
+++ routes/users.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var router = express.Router();
+var auth = require("../controller/auth");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+/* GET users listing. */
+router.post("/login", function (req, res, next) {
+  const username = req.body.username;
+
+  auth.login(username, req.body.password, function (loginResult) {
+    console.log("login result", loginResult);
+
+    if (loginResult && !loginResult.code) {
+      res.render("users", { username: username });
+    } else {
+      res.render("index", { error: true });
+    }
+  });
+});
+
+router.post("/authActions", function (req, res, next) {
+  console.log("requested action", req.body.authaction);
+
+  if (req.body.authaction && req.body.authaction == "logout") {
+    auth.logout(function (logoutResult) {
+      console.log("logout result", logoutResult);
+
+      if (logoutResult) {
+        res.render("index", { error: false });
+      } else {
+        res.render("users", { username: username });
+      }
+    });
+  }else if(req.body.authaction && req.body.authaction == "refresh"){
+    auth.refreshToken(function (refreshResult) {
+      console.log("refresh result", refreshResult);
+    });
+  }else if(req.body.authaction && req.body.authaction == "validate"){
+    auth.validateToken(function (validateResult) {
+      console.log("validate result", validateResult);
+    });
+  }
+});
+
+module.exports = router;
